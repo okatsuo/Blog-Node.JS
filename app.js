a0da8687f1655feacbfe54ad407e5242f3fbe35f
okatsuo <rafael.goncanna@gmail.com>
2021-04-06 15:35:53 -0300

    adicionado as dependências principais para o projeto e a estrutura de pastas
    adicionado as dependências:
    express - framework;
    express-handlebars - para templates;
    body-parser - acessar as variáveis na view;
    express-router - para controlar as rotas.
    adicionado a estrutura de pastas:
    models-  para os modelos;
    routes - para separar as rotas;
    views - para a criação das telas;
    public - para os arquivos js/css.

---
+++ app.js
@@ -0,0 +1,31 @@
+//carregando os módulos
+const express = require("express");
+const handlebars = require("express-handlebars");
+const bodyParser = require("body-parser");
+const app = express();
+const admin = require("./routes/admin");
+const path = require("path");
+//const mongoose = require("mongoose");
+
+//Configurações
+//body-parser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//handleBars
+app.engine("handlebars", handlebars({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+//mongoose
+
+// Public
+
+app.use(express.static(path.join(__dirname, "public")));
+
+//Rotas
+app.use("/admin", admin);
+//Outros
+const port = 8081;
+app.listen(port, () => {
+  console.log("servidor rodando!");
+});
