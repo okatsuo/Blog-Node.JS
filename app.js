91641c8dd7c03b447e49c1728e7f0b01de757a6b
okatsuo <rafael.goncanna@gmail.com>
2021-04-07 17:06:10 -0300

    configurando a sessão e o middleware

--- app.js
+++ app.js
@@ -6,8 +6,26 @@ const app = express();
 const admin = require("./routes/admin");
 const path = require("path");
 const mongoose = require("mongoose");
+const session = require("express-session");
+const flash = require("connect-flash");
 
 //Configurações
+//session
+app.use(
+  session({
+    secret: "segredosecreto",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+app.use(flash());
+//Middleware
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  next();
+});
+
 //body-parser
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
