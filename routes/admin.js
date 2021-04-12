dcd6f1ab93ccebb5473aae0e06ed90522360b56c
okatsuo <rafael.goncanna@gmail.com>
2021-04-12 12:06:00 -0300

    função de editar categoria

--- routes/admin.js
+++ routes/admin.js
@@ -76,4 +76,39 @@ router.post("/categorias/nova", (req, res) => {
   }
 });
 
+router.get("/categorias/edit/:id", (req, res) => {
+  Categoria.findOne({ _id: req.params.id })
+    .lean()
+    .then((categoria) => {
+      res.render("admin/editcategorias", { categoria: categoria });
+    })
+    .catch((err) => {
+      req.flash("error_msg", "Esta categoria não existe");
+      res.redirect("/admin/categorias");
+    });
+});
+
+router.post("/categorias/edit", (req, res) => {
+  Categoria.findOne({ _id: req.body.id })
+    .then((categoria) => {
+      categoria.nome = req.body.nome;
+      categoria.slug = req.body.slug;
+
+      categoria
+        .save()
+        .then(() => {
+          req.flash("success_msg", "editado com sucesso");
+          res.redirect("/admin/categorias");
+        })
+        .catch((err) => {
+          req.flash("error_msg", "houve um erro ao salvar a edição");
+          res.redirect("/admin/categorias");
+        });
+    })
+    .catch((err) => {
+      req.flash("error_msg", "houve um erro ao editar a categoria");
+      res.redirect("/admin/categorias");
+    });
+});
+
 module.exports = router;
